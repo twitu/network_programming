a4fe0de22e60ccecd7ca73283be929600e1ea820
Ishan Bhanuka <bhanuka.ishan@gmail.com>
2018-11-21 15:58:27 +0530

    in sequence event model without using explicit event queue

--- assignment_2/problem_2/event_driven_server.c
+++ assignment_2/problem_2/event_driven_server.c
@@ -41,6 +41,13 @@ int main() {
 
 void* event_generator(void* args) {
 
+    int bytes, conn;
+    char request[HTTP_REQ_LIMIT];
+    char resource[RESOURCE_LIMIT];
+
+    // initialize data structures to store data
+    create_data_store(MAX_CLIENTS);
+
     // create listening address to bind
     struct sockaddr_in listen_addr;
     listen_addr.sin_family = AF_INET;
@@ -51,13 +58,12 @@ void* event_generator(void* args) {
     int enable = 1;
     int listen_fd = socket(AF_INET, SOCK_STREAM, IPPROTO_TCP);
     setsockopt(listen_fd, SOL_SOCKET, SO_REUSEADDR, &enable, sizeof(int));
-    fcntl(listen_fd, F_SETFL, fcntl(listen_fd, F_GETFL, 0) | O_NONBLOCK);
+    fcntl(listen_fd, F_SETFL, 0);
     bind(listen_fd, (struct sockaddr*) &listen_addr, sizeof(struct sockaddr));
     listen(listen_fd, 5);
 
     // initialize structures for epoll
     struct epoll_event listener_event, events[MAX_CLIENTS + 1];
-    // events slot 0 is always assigned to listener fd
     listener_event.data.fd = listen_fd;
     listener_event.events = EPOLLIN | EPOLLET;
 
@@ -67,7 +73,8 @@ void* event_generator(void* args) {
     // register listener to epoll fd
     epoll_ctl(epfd, EPOLL_CTL_ADD, listen_fd, &listener_event);
 
-    int event_count;
+    int event_count, event_fd;
+    Data event_data;
     while (1) {
         event_count = epoll_wait(epfd, events, MAX_CLIENTS + 1, 10000);
         for (int i = 0; i < event_count; i++) {
@@ -75,17 +82,108 @@ void* event_generator(void* args) {
             #ifdef DEBUG
             printf("events to process %d\n", event_count);
             #endif
+
+            // accept connections
             if (events[i].data.fd == listen_fd) {
-                add_event(listen_fd, ACCEPT_EVENT);
-                #ifdef DEBUG
-                print_tail_event();
-                #endif
+                // if there is no space in data store accept and close connections
+                // this is done to avoid indefinetly blocking on listen_fd
+                // this is a common issue with edge triggered systems
+                while (!has_space()) {
+                    #ifdef DEBUG
+                    printf("no space dropping connections\n");
+                    #endif
+                    conn = accept(listen_fd, NULL, 0);
+                    close(conn);
+                }
+
+                while (1) {
+                    if ((conn = accept(listen_fd, NULL, 0)) == -1) {
+                        if (errno == EAGAIN | errno == EWOULDBLOCK) break;
+                        else perror("accept()");
+                    } else {
+                        #ifdef DEBUG
+                        printf("connected on socket %d\n", conn);
+                        #endif
+                        fcntl(conn, F_SETFL, fcntl(conn, F_GETFL, 0) | O_NONBLOCK);
+                        new_data_slot(conn);
+                        struct epoll_event new_event;
+                        new_event.data.fd = conn;
+                        new_event.events = EPOLLIN | EPOLLET;  // receive only read events to get complete http request
+                        epoll_ctl(epfd, EPOLL_CTL_ADD, conn, &new_event);
+                    }
+                }
             } else {
-                if (events[i].events == EPOLLIN) add_event(events[i].data.fd, READ_EVENT);
-                else if (events[i].events == EPOLLOUT) add_event(events[i].data.fd, WRITE_EVENT);
-                #ifdef DEBUG
-                print_tail_event();
-                #endif
+                if (events[i].events == EPOLLIN) {
+                    event_data = get_event_data(events[i].data.fd);
+                    // ignore if data not in READ_STATE
+                    if (event_data->state == READ_STATE) {    
+                        conn = events[i].data.fd;
+                        bytes = event_data->bytes_read;
+
+                        if ((bytes = recv(conn, &event_data->request[bytes], HTTP_REQ_LIMIT - bytes, 0)) == -1) {
+                            // read nothing
+                            if (errno == EAGAIN | errno == EWOULDBLOCK);
+                            else perror("recv()");
+                        } else {
+                            event_data->bytes_read += bytes;
+                            if (strstr(event_data->request, "\r\n\r\n")) {
+                                // request received
+                                event_data->state = PROCESS_STATE;
+                                sprintf(event_data->request, "GET /%s ", resource);
+                                if (strstr(resource, ".cgi") == NULL) {
+                                    // call helper thread to load file  
+                                    sscanf(event_data->resource, "%s", resource);
+
+                                    // map file to memory
+                                    int file_fd = open(resource, O_RDONLY);
+                                    struct stat file_stat;
+                                    fstat(file_fd, &file_stat);
+                                    event_data->mapped_file = (char*) mmap(NULL, file_stat.st_size, PROT_READ, MAP_PRIVATE, file_fd, 0);
+                                    mlock(event_data->mapped_file, file_stat.st_size);
+
+                                    // modify to write state and change socket events for OUT
+                                    event_data->state = WRITE_STATE;
+                                    struct epoll_event modify;
+                                    modify.data.fd = conn;
+                                    modify.events = EPOLLOUT | EPOLLET;
+                                    epoll_ctl(epfd, EPOLL_CTL_MOD, conn, &modify);
+                                    #ifdef DEBUG
+                                    printf("received complete request\n");
+                                    #endif
+                                } else {
+                                    // TODO call fast cgi process
+                                }
+                            }
+
+                            #ifdef DEBUG
+                            printf("read %d bytes\n", bytes);
+                            #endif
+                        }
+                    }
+                } else if (events[i].events == EPOLLOUT) {
+                    #ifdef DEBUG
+                    printf("write event\n");
+                    #endif
+
+                    event_data = get_event_data(events[i].data.fd);
+                    // ignore if data not in WRITE_STATE
+                    if (event_data->state == WRITE_STATE) {    
+                        conn = events[i].data.fd;
+                        bytes = event_data->bytes_written;
+
+                        if ((bytes = sendto(conn, &event_data->mapped_file[bytes], event_data->bytes_to_write - bytes, 0, NULL, 0)) == -1) {
+                            if (errno == EAGAIN | errno == EWOULDBLOCK);
+                            else perror("send()");
+                        } else {
+                            event_data->bytes_written += bytes;
+                            if (event_data->bytes_to_write == event_data->bytes_written) {
+                                close(conn);
+                                munlock(event_data->mapped_file, event_data->bytes_to_write);
+                                delete_event_data(conn);
+                            }
+                        }
+                    }
+                }
             }
         }
     }
